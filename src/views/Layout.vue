ea99fb991fab75ef0dc009e6c3df69a797e66c4b
GeekBen <58085975+GeekBenCN@users.noreply.github.com>
2020-06-17 00:53:53 +0800

    update

---
+++ src/views/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Head></Head>
+    <section class="section">
+      <div class="container">
+        <!-- <keep-alive> -->
+        <feb-alive>
+          <router-view></router-view>
+        </feb-alive>
+        <!-- </keep-alive> -->
+        <Footer></Footer>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Head from "./common/Head";
+import Footer from "./common/Footer";
+export default {
+  data: function () {
+    return {};
+  },
+  components: {
+    Head,
+    Footer: Footer,
+  },
+  methods: {},
+};
+</script>
